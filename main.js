dd0e2688d85a31d21c77230e3d18f593fedeb05a
paulkko <kunpaulko@gmail.com>
2025-03-28 02:22:38 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const aboutTag = document.getElementById("about");
+const expTag = document.getElementById("experience");
+const projTag = document.getElementById("projects");
+
+const conTag = document.getElementById("container")
+
+function changeText() {
+    const conTag = document.getElementById("container")
+
+    conTag.innerText = ""
+}
